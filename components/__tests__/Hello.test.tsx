fd7e177857d3acee56c7fbe248e4e3a7ad8c6f6c
kwiecien <k.d.kwiecinski@gmail.com>
2019-03-28 14:22:25 +0100

    add Jest

--- components/__tests__/Hello.test.tsx
+++ components/__tests__/Hello.test.tsx
@@ -4,6 +4,6 @@ import renderer from 'react-test-renderer'
 import { Hello } from "../Hello"
 
 it("renders correctly with defaults", () => {
-    const button = renderer.create(<Hello name="World" enthusiasmLevel={1} />).toJSON()
+    const button = renderer.create(<Hello name="World" enthusiasmLevel={1} />).toJSON();
     expect(button).toMatchSnapshot()
 });
